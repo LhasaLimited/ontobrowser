9963fde80963cec4440270b87ae52d95a9bb2168
Artur Polit <artur.polit@lhasalimited.org>
2017-06-23 11:39:32 +0200

    OWLAPI version update: OBOVocalbulary import fix 2

--- src/main/java/com/novartis/pcs/ontology/service/parser/obo/TypeDefNameTagHandler.java
+++ src/main/java/com/novartis/pcs/ontology/service/parser/obo/TypeDefNameTagHandler.java
@@ -1,6 +1,6 @@
 package com.novartis.pcs.ontology.service.parser.obo;
 
-import org.coode.owlapi.obo.parser.OBOVocabulary;
+import org.coode.owlapi.obo12.parser.OBOVocabulary;
 
 import com.novartis.pcs.ontology.entity.RelationshipType;
 
