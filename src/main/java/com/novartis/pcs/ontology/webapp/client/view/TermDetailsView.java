546115ca4363d36ec0af4bf983b4cce65e702994
Artur Polit <artur.polit@lhasalimited.org>
2017-08-08 12:50:33 +0200

    display label of annotations in Annotations tab
    Former-commit-id: 6de799c6229d2a17ba0419eac90e82c5039cb624

--- src/main/java/com/novartis/pcs/ontology/webapp/client/view/TermDetailsView.java
+++ src/main/java/com/novartis/pcs/ontology/webapp/client/view/TermDetailsView.java
@@ -26,6 +26,7 @@ import com.google.gwt.user.client.ui.HTMLTable.CellFormatter;
 import com.google.gwt.user.client.ui.SimplePanel;
 import com.google.gwt.user.client.ui.TabPanel;
 import com.novartis.pcs.ontology.entity.Annotation;
+import com.novartis.pcs.ontology.entity.AnnotationType;
 import com.novartis.pcs.ontology.entity.CrossReference;
 import com.novartis.pcs.ontology.entity.Term;
 import com.novartis.pcs.ontology.webapp.client.OntoBrowserServiceAsync;
@@ -39,38 +40,37 @@ import com.novartis.pcs.ontology.webapp.client.event.ViewTermEvent;
 import com.novartis.pcs.ontology.webapp.client.event.ViewTermHandler;
 import com.novartis.pcs.ontology.webapp.client.util.UrlValidator;
 
-public class TermDetailsView extends OntoBrowserView implements ViewTermHandler, 
-		EntityUpdatedHandler<Term>, EntityDeletedHandler<Term> {
-	private final String[] LABELS = {
-			"Id", "Ontology", "Term", "Definition", "Source", "Reference Id", "Comments", "Created By", "Status" 
-		};
-		
-	private final SimplePanel simplePanel = new SimplePanel(); 
+public class TermDetailsView extends OntoBrowserView
+		implements ViewTermHandler, EntityUpdatedHandler<Term>, EntityDeletedHandler<Term> {
+	private final String[] LABELS = { "Id", "Ontology", "Term", "Definition", "Source", "Reference Id", "Comments",
+			"Created By", "Status" };
+
+	private final SimplePanel simplePanel = new SimplePanel();
 	private final TabPanel tabPanel = new TabPanel();
 	private final Grid grid = new Grid(LABELS.length, 2);
-	private final Grid annotationGrid = new Grid(0,2);
+	private final Grid annotationGrid = new Grid(0, 2);
 	private Term currentTerm;
-		
+
 	public TermDetailsView(EventBus eventBus, OntoBrowserServiceAsync service) {
 		super(eventBus, service);
-		
-		for(int i = 0; i < LABELS.length; i++) {
+
+		for (int i = 0; i < LABELS.length; i++) {
 			grid.setText(i, 0, LABELS[i] + ":");
 		}
 		grid.setWidth("100%");
-				
+
 		setLabelStyle(grid);
-		
+
 		tabPanel.add(grid, "General");
-		tabPanel.add(annotationGrid, "Annotations");		
+		tabPanel.add(annotationGrid, "Annotations");
 		tabPanel.setWidth("100%");
 		tabPanel.selectTab(0);
-		
+
 		simplePanel.add(tabPanel);
 		initWidget(simplePanel);
-		
+
 		addStyleName("padded-border vert-scroll fixed-height");
-		
+
 		eventBus.addHandler(ViewTermEvent.TYPE, this);
 		eventBus.addHandler(TermUpdatedEvent.TYPE, this);
 		eventBus.addHandler(TermDeletedEvent.TYPE, this);
@@ -81,26 +81,24 @@ public class TermDetailsView extends OntoBrowserView implements ViewTermHandler,
 		currentTerm = event.getTerm();
 		int row = 0;
 		grid.setText(row++, 1, currentTerm.getReferenceId());
-		grid.setText(row, 0, currentTerm.getOntology().isCodelist() ?
-				"Codelist:" : "Ontology:");	
+		grid.setText(row, 0, currentTerm.getOntology().isCodelist() ? "Codelist:" : "Ontology:");
 		grid.setText(row++, 1, currentTerm.getOntology().getName());
-		if(UrlValidator.validate(currentTerm.getUrl())) {
+		if (UrlValidator.validate(currentTerm.getUrl())) {
 			Anchor a = new Anchor(currentTerm.getName(), currentTerm.getUrl().trim());
 			a.setTarget("TermUrl");
 			grid.setWidget(row++, 1, a);
 		} else {
 			grid.setText(row++, 1, currentTerm.getName());
 		}
-		
+
 		grid.setText(row++, 1, currentTerm.getDefinition());
-		
+
 		boolean xrefPresent = false;
-		for(CrossReference xref : currentTerm.getCrossReferences()) {
-			if(!xref.isDefinitionCrossReference()
-					&& xref.getDatasource() != null) {
+		for (CrossReference xref : currentTerm.getCrossReferences()) {
+			if (!xref.isDefinitionCrossReference() && xref.getDatasource() != null) {
 				grid.setText(row++, 1, xref.getDatasource().getAcronym());
-				
-				if(xref.getReferenceId() != null) {
+
+				if (xref.getReferenceId() != null) {
 					grid.setText(row++, 1, xref.getReferenceId());
 				} else {
 					grid.setText(row++, 1, null);
@@ -109,52 +107,52 @@ public class TermDetailsView extends OntoBrowserView implements ViewTermHandler,
 				break;
 			}
 		}
-		
-		if(!xrefPresent) {
+
+		if (!xrefPresent) {
 			grid.setText(row++, 1, null);
 			grid.setText(row++, 1, null);
 		}
-		
+
 		grid.setText(row++, 1, currentTerm.getComments());
 		grid.setText(row++, 1, currentTerm.getCreatedBy().getUsername());
 		grid.setText(row++, 1, currentTerm.getStatus().toString());
-		
+
 		fillAnnotations(currentTerm.getAnnotations());
 	}
 
 	private void fillAnnotations(Set<Annotation> annotations) {
 		annotationGrid.clear();
 		annotationGrid.resizeRows(annotations.size());
-		tabPanel.getTabBar().setTabText(1, "Annotations ("+annotations.size()+")");
-		
-		int row = 0;		
-		for (Annotation annotation : annotations) {		
-			annotationGrid.setText(row, 0, annotation.getAnnotationType().getPrefixedXmlType() + ":");
+		tabPanel.getTabBar().setTabText(1, "Annotations (" + annotations.size() + ")");
+
+		int row = 0;
+		for (Annotation annotation : annotations) {
+			AnnotationType annotationType = annotation.getAnnotationType();
+			annotationGrid.setText(row, 0, annotationType.getLabel());
 			annotationGrid.setText(row, 1, annotation.getAnnotation());
 			row++;
-		}		
-		
+		}
+
 		setLabelStyle(annotationGrid);
 	}
-
 
 	@Override
 	public void onEntityUpdated(EntityUpdatedEvent<Term> event) {
 		Term term = event.getEntity();
-		if(currentTerm != null && currentTerm.equals(term)) {
+		if (currentTerm != null && currentTerm.equals(term)) {
 			onViewTerm(new ViewTermEvent(term));
 		}
 	}
 
 	@Override
 	public void onEntityDeleted(EntityDeletedEvent<Term> event) {
-				
+
 	}
-	
+
 	private void setLabelStyle(Grid gridWithLabels) {
 		int row;
-		CellFormatter cellFormatter = gridWithLabels.getCellFormatter(); 
-		for(row = 0; row < annotationGrid.getRowCount(); row++) {
+		CellFormatter cellFormatter = gridWithLabels.getCellFormatter();
+		for (row = 0; row < annotationGrid.getRowCount(); row++) {
 			cellFormatter.addStyleName(row, 0, "label");
 		}
 	}
